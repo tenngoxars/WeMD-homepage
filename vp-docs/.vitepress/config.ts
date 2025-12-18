7c5a6ab3fc9dd065172fdccff748e41d0bd668c3
tenngo <tenngoxars@hotmail.com>
2025-12-18 18:43:08 +0800

    feat: update navigation text from "使用指南" to "帮助文档", add new dark/light mode logos, and configure navigation links.

--- vp-docs/.vitepress/config.ts
+++ vp-docs/.vitepress/config.ts
@@ -17,7 +17,7 @@ export default defineConfig({
     ],
 
     themeConfig: {
-        logo: 'logo.svg',
+        logo: { light: '/logo-dark.svg', dark: '/logo-light.svg' },
 
         nav: [
             { text: '首页', link: 'https://wemd.app' },
